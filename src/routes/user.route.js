3bf36a64ef744044667da6526e11c279da190e98
Hardik gupta <hardikgupta3003@gmail.com>
2024-02-26 20:56:52 +0530

    update user controller

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,14 @@
 import { Router } from "express";
-import { registerUser,loginUser,logoutUser,refreshAccessToken } from "../controllers/user.controller.js";
+import {
+    registerUser,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails
+
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -8,17 +17,21 @@ const router = Router();
 router.route("/register").post(
     upload.fields([
         {
-            name:"avatar",
-            maxCount:1
+            name: "avatar",
+            maxCount: 1
         },
         {
-            name:"coverImage",
-            maxCount:1
+            name: "coverImage",
+            maxCount: 1
         }
     ]),
     registerUser);
 
 router.route("/login").post(loginUser);
-router.route("/logout").post(verifyJWT,logoutUser);
-router.route("/refresh-Token").post(refreshAccessToken)
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-Token").post(refreshAccessToken);
+router.route("/change-Password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+
 export default router
